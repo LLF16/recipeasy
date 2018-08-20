c8c244edb9fdc279c126037d1b93bb86e1e6281f
Louis Masset <lmasset16@gmail.com>
2018-08-20 17:02:24 +0200

    implement recipes index basic

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,16 +2,17 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:query].present?
+    if params.present?
       sql_query = "\
-      ingredients.name ILIKE :query1 \
-      ingredients.name ILIKE :query2 \
-      ingredients.name ILIKE :query3 \
+      ingredients.name ILIKE :query1 AND\
+      ingredients.name ILIKE :query2 AND\
+      ingredients.name ILIKE :query3 AND\
       ingredients.name ILIKE :query4 \
       "
-      @recipes = Recipe.joins(:ingredients).where(sql_query, query1: "#{params[:ingredient_1]}", query2: "#{params[:ingredient_2]}",query3: "#{params[:ingredient_3]}",query4: "#{params[:ingredient_4]}")
-      @selected_recipe = @recipes.take(3)
+      @recipes = Recipe.joins(:ingredients).where(sql_query, query1: params["ingredient_1"], query2: params["ingredient_2"],query3: params["ingredient_3"],query4: params["ingredient_4"])
+      @selected_recipes = @recipes.take(3)
     else
+      raise
       @recipes = Recipe.all
     end
   end
