28130b0d77d580b4b0d333db7359422f3654fa23
Diego Castellaro <die.cst@gmail.com>
2018-08-20 18:15:00 +0200

    autocomplete basic

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,4 +5,9 @@ class Ingredient < ApplicationRecord
   has_many :recipes, through: :measurements
 
   enum category: [:base, :topping, :side]
+
+  def.self.search(term)
+    Ingredient.where('name ILIKE ?',"%#{term.downcase}%")
+  end
+
 end
