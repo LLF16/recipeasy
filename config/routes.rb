3220e9146a1844e34aafbaca1a1e680bf38c4894
Jascha Rafael Franke <hola@11once.de>
2018-08-20 18:41:36 +0200

    Implement user show page with selected recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
   devise_for :users
   root to: 'ingredients#index'
   resources :recipes, only: [:index]
+  resources :profiles, only: [:show]
 end
