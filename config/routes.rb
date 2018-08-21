37b05b10c097f6a7512509e53e08b806d4634677
Jascha Rafael Franke <hola@11once.de>
2018-08-21 10:18:47 +0200

    Add show to recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'profiles/show'
   devise_for :users
   root to: 'ingredients#index'
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :show ]
   resources :profiles, only: [:show]
   resources :cooking_sessions, only: :show
 end
