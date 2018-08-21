24c0860ab91cb08ecbf83c839224ce776fe4395c
Matt Pitt <matt.ianpitt@gmail.com>
2018-08-21 12:07:44 +0200

    links for cards

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,9 +4,9 @@ class RecipesController < ApplicationController
   def index
     if params.present?
       sql_query = "\
-      ingredients.name ILIKE :query1 AND\
-      ingredients.name ILIKE :query2 AND\
-      ingredients.name ILIKE :query3 AND\
+      ingredients.name ILIKE :query1 OR\
+      ingredients.name ILIKE :query2 OR\
+      ingredients.name ILIKE :query3 OR\
       ingredients.name ILIKE :query4 \
       "
       @recipes = Recipe.joins(:ingredients).where(sql_query, query1: params["ingredient_1"], query2: params["ingredient_2"],query3: params["ingredient_3"],query4: params["ingredient_4"])
