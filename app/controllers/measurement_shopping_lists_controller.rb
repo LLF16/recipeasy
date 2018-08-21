86ec1133a131b5ac13dce88337988bfaf1454b84
Diego Castellaro <die.cst@gmail.com>
2018-08-21 18:28:36 +0200

    redirect working + cleanup

--- app/controllers/measurement_shopping_lists_controller.rb
+++ app/controllers/measurement_shopping_lists_controller.rb
@@ -2,22 +2,15 @@ class MeasurementShoppingListsController < ApplicationController
 before_action :set_sl_measurement
 
   def destroy
+    @shopping_list = @sl_measurement.shopping_list
     if @sl_measurement.destroy
-      # redirect_to user_shopping_list_path(@user, @shopping_list)
+      redirect_to shopping_list_path(@shopping_list)
     else
       render :new
     end
   end
 
   private
-
-  def set_user
-    @user = current_user
-  end
-
-  def set_shopping_list
-    @shopping_list = ShoppingList.find(params[:shopping_list_id])
-  end
 
   def set_sl_measurement
     @sl_measurement = MeasurementShoppingList.find(params[:id])
