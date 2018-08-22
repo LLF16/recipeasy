5934a1d3a1be7e1850a6bf5b9a1323ad8b15b448
Louis Masset <lmasset16@gmail.com>
2018-08-22 19:11:28 +0200

    basic styling home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'search/index'
   devise_for :users
-  root to: 'ingredients#index'
-  resources :recipes, only: [:index, :show ]
+  root to: 'pages#home'
+  resources :ingredients, only: :index
+  resources :recipes, only: [:index, :show]
   resources :cooking_sessions, only: :show
   resources :profiles, only: [:show]
   resources :shopping_lists, only: [:show]
