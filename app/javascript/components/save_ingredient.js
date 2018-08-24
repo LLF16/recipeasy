8edea011b22e689858f6f40987d7c2db6f4114ad
Diego Castellaro <die.cst@gmail.com>
2018-08-24 15:01:07 +0200

    fixed

--- app/javascript/components/save_ingredient.js
+++ app/javascript/components/save_ingredient.js
@@ -32,7 +32,10 @@ document.body.onkeyup = function(e){
   let array = [];
   if(e.keyCode == 32){
     let ingredients = document.querySelectorAll('input.form-control');
+
     let locked_ingredients = ingredients.forEach((item) => {
+      // console.log(item)
+      // console.log(item.defaultValue)
       if(item.disabled) {
         array.push(item.defaultValue);
       } else {
@@ -46,13 +49,18 @@ document.body.onkeyup = function(e){
   fetch(url)
     .then(response => response.json())
     .then((data) => {
-      console.log("result", data);
-      console.log(data.first.name);
-      document.querySelector('.first-text-tag').value = data.first.name;
-      document.querySelector('.second-text-tag').value = data.second.name;
-      document.querySelector('.third-text-tag').value = data.third.name;
-      document.querySelector('.fourth-text-tag').value = data.fourth.name;
-    })
+      let data_array = [];
+      data_array.push(data.first);
+      data_array.push(data.second);
+      data_array.push(data.third);
+      data_array.push(data.fourth);
+
+      let counter = 0;
+      ingredients.forEach((item) => {
+        item.defaultValue = data_array[counter].name;
+        counter ++;
+      });
+    });
 
 
   };
