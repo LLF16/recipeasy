6883da6bfb07362a6666a1e7cf8605ab0039139c
Diego Castellaro <die.cst@gmail.com>
2018-08-28 16:29:03 +0200

    add some comments

--- app/javascript/components/replace_item.js
+++ app/javascript/components/replace_item.js
@@ -1,8 +1,6 @@
-
-
+// the first line is awaiting for change in the first input to enable the locking mechanism
+// so if nothing is in the field you can't lock it
 document.querySelectorAll('input.form-control')[0].addEventListener("change", (event) => {
-// if (document.querySelectorAll('input.form-control')[0].value !== "") {
-
   const unlockedLock1 = document.querySelector("#ingredients-index-1 .fas");
   unlockedLock1.addEventListener("click", (event) => {
     const text_field = document.querySelector("#ingredient_1");
@@ -82,53 +80,53 @@ document.querySelectorAll('input.form-control')[0].addEventListener("change", (e
   }
 });
 
-const unlockedLock2 = document.querySelector("#ingredients-index-2 .fas");
-unlockedLock2.addEventListener("click", (event) => {
-  const text_field = document.querySelector("#ingredient_2");
-  if (unlockedLock2.dataset.locked === 'false') {
-    unlockedLock2.dataset.locked = 'true';
-    unlockedLock2.classList.add("fa-lock");
-    unlockedLock2.classList.remove("fa-lock-open");
-    text_field.setAttribute("disabled", "true");
-  } else {
-    unlockedLock2.dataset.locked = 'false';
-    unlockedLock2.classList.remove("fa-lock");
-    unlockedLock2.classList.add("fa-lock-open");
-    text_field.removeAttribute("disabled");
-  }
-});
+  const unlockedLock2 = document.querySelector("#ingredients-index-2 .fas");
+  unlockedLock2.addEventListener("click", (event) => {
+    const text_field = document.querySelector("#ingredient_2");
+    if (unlockedLock2.dataset.locked === 'false') {
+      unlockedLock2.dataset.locked = 'true';
+      unlockedLock2.classList.add("fa-lock");
+      unlockedLock2.classList.remove("fa-lock-open");
+      text_field.setAttribute("disabled", "true");
+    } else {
+      unlockedLock2.dataset.locked = 'false';
+      unlockedLock2.classList.remove("fa-lock");
+      unlockedLock2.classList.add("fa-lock-open");
+      text_field.removeAttribute("disabled");
+    }
+  });
 
-const unlockedLock3 = document.querySelector("#ingredients-index-3 .fas");
-unlockedLock3.addEventListener("click", (event) => {
-  const text_field = document.querySelector("#ingredient_3");
-  if (unlockedLock3.dataset.locked === 'false') {
-    unlockedLock3.dataset.locked = 'true';
-    unlockedLock3.classList.add("fa-lock");
-    unlockedLock3.classList.remove("fa-lock-open");
-    text_field.setAttribute("disabled", "true");
-  } else {
-    unlockedLock3.dataset.locked = 'false';
-    unlockedLock3.classList.remove("fa-lock");
-    unlockedLock3.classList.add("fa-lock-open");
-    text_field.removeAttribute("disabled");
-  }
-});
+  const unlockedLock3 = document.querySelector("#ingredients-index-3 .fas");
+  unlockedLock3.addEventListener("click", (event) => {
+    const text_field = document.querySelector("#ingredient_3");
+    if (unlockedLock3.dataset.locked === 'false') {
+      unlockedLock3.dataset.locked = 'true';
+      unlockedLock3.classList.add("fa-lock");
+      unlockedLock3.classList.remove("fa-lock-open");
+      text_field.setAttribute("disabled", "true");
+    } else {
+      unlockedLock3.dataset.locked = 'false';
+      unlockedLock3.classList.remove("fa-lock");
+      unlockedLock3.classList.add("fa-lock-open");
+      text_field.removeAttribute("disabled");
+    }
+  });
 
-const unlockedLock4 = document.querySelector("#ingredients-index-4 .fas");
-unlockedLock4.addEventListener("click", (event) => {
-  const text_field = document.querySelector("#ingredient_4");
-  if (unlockedLock4.dataset.locked === 'false') {
-    unlockedLock4.dataset.locked = 'true';
-    unlockedLock4.classList.add("fa-lock");
-    unlockedLock4.classList.remove("fa-lock-open");
-    text_field.setAttribute("disabled", "true");
-  } else {
-    unlockedLock4.dataset.locked = 'false';
-    unlockedLock4.classList.remove("fa-lock");
-    unlockedLock4.classList.add("fa-lock-open");
-    text_field.removeAttribute("disabled");
-  }
-});
+  const unlockedLock4 = document.querySelector("#ingredients-index-4 .fas");
+  unlockedLock4.addEventListener("click", (event) => {
+    const text_field = document.querySelector("#ingredient_4");
+    if (unlockedLock4.dataset.locked === 'false') {
+      unlockedLock4.dataset.locked = 'true';
+      unlockedLock4.classList.add("fa-lock");
+      unlockedLock4.classList.remove("fa-lock-open");
+      text_field.setAttribute("disabled", "true");
+    } else {
+      unlockedLock4.dataset.locked = 'false';
+      unlockedLock4.classList.remove("fa-lock");
+      unlockedLock4.classList.add("fa-lock-open");
+      text_field.removeAttribute("disabled");
+    }
+  });
 
 });
 
