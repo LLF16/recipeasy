ef5c4b71c5834f2c7b84ebb7ae032806043b7ac8
Jascha Rafael Franke <hola@11once.de>
2018-08-29 15:48:57 +0200

    Fix migration error

--- db/migrate/20180820110629_create_ingredients.rb
+++ db/migrate/20180820110629_create_ingredients.rb
@@ -1,7 +1,6 @@
 class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
-      t.references :ingredient_family, foreign_key: true
       t.string :name
       t.string :photo
       t.boolean :vegan
