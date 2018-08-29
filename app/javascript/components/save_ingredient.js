ee3f20036a544d09b6aa01637bd945d8b88b07ab
Diego Castellaro <die.cst@gmail.com>
2018-08-29 11:41:26 +0200

    copied logic to shuffle button

--- app/javascript/components/save_ingredient.js
+++ app/javascript/components/save_ingredient.js
@@ -32,57 +32,58 @@
 let shuffleButton = document.querySelector(".ingredients-shuffle-button");
 shuffleButton.addEventListener("click", (event) => {
   let array = [];
-  // let first_ingredient = document.querySelectorAll('.form-control')[0].value
-  let ingredients = document.querySelectorAll('.form-control');
-  console.log(ingredients)
-  let ingredientNames = document.querySelectorAll('.slot-ingredient-name');
-  let locked_ingredients = ingredients.forEach((item) => {
-    if(item.disabled) {
-      array.push(item.value);
-    } else {
-      array.push("empty");
-    };
-  });
-  console.log(array)
-  let url = `search/ingredients?first=${array[0]}&second=${array[1]}&third=${array[2]}&fourth=${array[3]}`;
-  console.log(url);
-  fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      let data_array = [];
-      let backgrounds = document.querySelectorAll('.ingredients-index-bar');
-      data_array.push(data.first);
-      data_array.push(data.second);
-      data_array.push(data.third);
-      data_array.push(data.fourth);
-
-      let counter = 0;
-      ingredientNames.forEach((item) => {
-        item.innerText = data_array[counter].name;
-        counter ++;
-      });
-
-      // old logic that changes the value inside the text input. Need to keep it
-      // cause the locking mechanism still works on the input field (that are just hidden)
-      let counter_1 = 0;
-      ingredients.forEach((item) => {
-        item.value = data_array[counter_1].name;
-        counter_1 ++;
-      });
-
-
-
-      let second_counter = 0;
-      backgrounds.forEach((item) => {
-        console.log(second_counter);
-        // item.style.backgroundImage = `"url('${data_array[second_counter].photo.url}')"`;
-        item.style.backgroundImage = 'url(' + data_array[second_counter].photo.url + ')';
-
-        console.log(item.style.backgroundImage);
-        second_counter ++;
-      });
+    let ingredients = document.querySelectorAll('.form-control');
+    // let first_ingredient = document.querySelectorAll('.form-control')[0].value
+    let ingredientNames = document.querySelectorAll('.slot-ingredient-name');
+    let locked_ingredients = ingredients.forEach((item) => {
+      console.log(item.classList.contains("disabled"))
+      if(item.classList.contains("disabled")) {
+        array.push(item.value);
+      } else {
+        array.push("empty");
+      };
     });
+    // array.unshift(first_ingredient)
+    console.log(array)
+    let url = `search/ingredients?first=${array[0]}&second=${array[1]}&third=${array[2]}&fourth=${array[3]}`;
+    console.log(url);
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        let data_array = [];
+        let backgrounds = document.querySelectorAll('.ingredients-index-bar');
+        data_array.push(data.first);
+        data_array.push(data.second);
+        data_array.push(data.third);
+        data_array.push(data.fourth);
+
+        let counter = 0;
+        ingredientNames.forEach((item) => {
+          item.innerText = data_array[counter].name;
+          counter ++;
+        });
+
+        // old logic that changes the value inside the text input. Need to keep it
+        // cause the locking mechanism still works on the input field (that are just hidden)
+        let counter_1 = 0;
+        ingredients.forEach((item) => {
+          item.value = data_array[counter_1].name;
+          counter_1 ++;
+        });
+
+
+
+        let second_counter = 0;
+        backgrounds.forEach((item) => {
+          console.log(second_counter);
+          // item.style.backgroundImage = `"url('${data_array[second_counter].photo.url}')"`;
+          item.style.backgroundImage = 'url(' + data_array[second_counter].photo.url + ')';
+
+          console.log(item.style.backgroundImage);
+          second_counter ++;
+        });
+      });
 });
 
 
