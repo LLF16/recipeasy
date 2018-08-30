4e232599df989fbb420e55aa4708a9e29999c3d8
Diego Castellaro <die.cst@gmail.com>
2018-08-30 12:24:30 +0200

    add cookie for tutorial

--- app/javascript/components/overlay_ingredient_index.js
+++ app/javascript/components/overlay_ingredient_index.js
@@ -1,6 +1,8 @@
-document.getElementById("activate-overlay").addEventListener("click", () => {
-      document.getElementById("overlay").style.display = "block";
-  });
- document.getElementById("overlay").addEventListener("click", () => {
-      document.getElementById("overlay").style.display = "none";
-  });
+if (document.cookie.includes("overlay")) {
+  document.getElementById("overlay").style.display = "none";
+};
+
+document.getElementById("overlay").addEventListener("click", () => {
+  document.getElementById("overlay").style.display = "none";
+  document.cookie = "overlay=seen";
+});
